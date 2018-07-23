22304f725f83a47cc961d88ebf379b27f0a400ed
KaumudiMummadi <kaumudi.mummadi@gmail.com>
2018-07-23 21:13:13 +0000

    automatically backed up by learn

--- macarena.rb
+++ macarena.rb
@@ -25,7 +25,7 @@ puts "Step 5: "Place your right hand on your left shoulder."
 puts "Step 6: "Place your left hand on your right shoulder."
 
 # Step 7: FIXME
-puts "Step 7:"Bring your right hand up and around to the back of your head."
+puts "Step 7: "Bring your right hand up and around to the back of your head."
 
 # Step 8: FIXME
-puts "Bring your left hand up and around to the back your head"
+puts "Bring your left hand up and around to the back your head."
